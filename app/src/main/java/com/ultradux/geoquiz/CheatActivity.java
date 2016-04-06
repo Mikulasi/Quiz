ab062b0e39ece83612089833109e2882430c5cf8
Mikalai Yakauleu <mikulas.yakovlev@gmail.com>
2016-04-06 19:35:55 +0300

    final commit

--- app/src/main/java/com/ultradux/geoquiz/CheatActivity.java
+++ app/src/main/java/com/ultradux/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.ultradux.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,8 +16,10 @@ public class CheatActivity extends AppCompatActivity {
 
     public static final String EXTRA_ANSWER_IS_TRUE = "com.ultradux.quiz.answer_is_true";
     public static final String EXTRA_ANSWER_SHOW = "com.ultradux.quiz.answer_shown";
+    public static final String KEY_INDEX = "index";
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
+    private TextView mVersionSdkTextView;
     private Button mShowAnswer;
     private boolean mAnswerShownResult;
 
@@ -25,6 +31,7 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+        mVersionSdkTextView = (TextView) findViewById(R.id.version_sdk);
 
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,8 +42,40 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        if (savedInstanceState != null) {
+            mAnswerShownResult = savedInstanceState.getBoolean(KEY_INDEX);
+        }
+
+        String versionSdk = getString(R.string.api_level) + Build.VERSION.SDK_INT;
+        mVersionSdkTextView.setText(versionSdk);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_INDEX, mAnswerShownResult);
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
--- app/src/main/java/com/ultradux/geoquiz/QuizActivity.java
+++ app/src/main/java/com/ultradux/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.ultradux.geoquiz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
